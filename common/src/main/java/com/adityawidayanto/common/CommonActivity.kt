b85ad599ec020ca9dbcacd2828bb3009dc07ba07
Aditya Widayanto <v-aditya.widayanto@anteraja.id>
2021-04-22 15:40:47 +0700

    add new module

---
+++ common/src/main/java/com/adityawidayanto/common/CommonActivity.kt
@@ -0,0 +1,11 @@
+package com.adityawidayanto.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common)
+    }
+}
---
+++ core/src/main/java/com/adityawidayanto/core/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.adityawidayanto.core.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adityawidayanto.core.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
