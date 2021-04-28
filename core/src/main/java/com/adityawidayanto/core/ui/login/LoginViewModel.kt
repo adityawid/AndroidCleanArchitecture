3a42662b8ce6d2c158c85522302f694309f5c619
Aditya Widayanto <v-aditya.widayanto@anteraja.id>
2021-04-28 15:14:23 +0700

    login

---
+++ core/src/main/java/com/adityawidayanto/core/ui/login/LoginViewModel.kt
@@ -0,0 +1,13 @@
+package com.adityawidayanto.core.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+
+
+class LoginViewModel (
+//        private val getTvShowsUseCase: GetTvShowsUseCase,
+//        private val updateTvShowsUseCase: UpdateTvShowsUseCase
+) : ViewModel() {
+
+
+}
